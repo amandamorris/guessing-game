d71234aff796a1d97a68d198c9a9b0ee50e70508
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 12:57:56 -0800

    Player greeting -2: get 1st guess

--- game.py
+++ game.py
@@ -2,3 +2,8 @@
 print "Hi there!"
 
 user_name = raw_input("What's your name? ")
+
+print "Hi there {}, would you like to play number guessing game?".format(user_name)
+print "I'll think of a number, and you can try to guess it."
+
+user_gues = int(raw_input("Please choose a number between 1 and 100. "))
