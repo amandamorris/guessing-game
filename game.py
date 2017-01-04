c0fc92bda2a8dc14b3a3b3dc5e9e17822904110b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 16:12:46 -0800

    bare bones

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 # Put your code here
+from random import randint
+
 print "Hi there!"
 
 user_name = raw_input("What's your name? ")
@@ -6,4 +8,17 @@ user_name = raw_input("What's your name? ")
 print "Hi there {}, would you like to play number guessing game?".format(user_name)
 print "I'll think of a number, and you can try to guess it."
 
-user_gues = int(raw_input("Please choose a number between 1 and 100. "))
+user_guess = int(raw_input("Please choose a number between 1 and 100. "))
+
+random_number = randint(1, 100)
+num_guesses = 0
+
+while user_guess != random_number:
+    if user_guess < random_number:
+        print "too low message"
+        user_guess = int(raw_input("Choose again. "))
+    else:
+        print "too high message"
+        user_guess = int(raw_input("Choose again. "))
+
+print "congrats message"
