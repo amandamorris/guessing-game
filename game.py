2f5078bb576207823ff23229ef584344cfdf41e6
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 17:08:06 -0800

    adding check that user guess is in range

--- game.py
+++ game.py
@@ -11,15 +11,17 @@ print "I'll think of a number, and you can try to guess it."
 user_guess = int(raw_input("Please choose a number between 1 and 100. "))
 
 random_number = randint(1, 100)
-print random_number
+#print random_number
 
 num_guesses = 1
 
 while user_guess != random_number:
-    if user_guess < random_number:
+    if 0 < user_guess and user_guess < random_number:
         print "Too low! "
-    else:
+    elif user_guess > random_number and user_guess < 101:
         print "Too high!"
+    else:
+        print "Try again with a number that's actually between 1 and 100"
     user_guess = int(raw_input("Please choose again. "))
     num_guesses += 1
 
