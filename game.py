a572b9459bc796a1933fc1d088c9dddfea5bff26
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-03 16:56:56 -0800

    refactored while-loop

--- game.py
+++ game.py
@@ -11,18 +11,16 @@ print "I'll think of a number, and you can try to guess it."
 user_guess = int(raw_input("Please choose a number between 1 and 100. "))
 
 random_number = randint(1, 100)
-#print random_number
+print random_number
 
 num_guesses = 1
 
 while user_guess != random_number:
     if user_guess < random_number:
         print "Too low! "
-        user_guess = int(raw_input("Please choose again. "))
-        num_guesses += 1
     else:
         print "Too high!"
-        user_guess = int(raw_input("Please choose again. "))
-        num_guesses += 1
+    user_guess = int(raw_input("Please choose again. "))
+    num_guesses += 1
 
 print "Congrats! You guessed it. You made {} guesses.".format(num_guesses)
